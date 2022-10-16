7fb92526d121ce823032bae182e04ed0d6538f69
Madelyn Lease <madelyn.lease@wsu.edu>
2022-10-16 13:11:30 -0400

    updated HTML and CSS per assignment, emptied existing window.onload event

--- scripts.js
+++ scripts.js
@@ -30,35 +30,7 @@ function loadFileInto(fromFile, whereTo) {
 
 window.onload = function() {
  
-  loadFileInto("ingredients.html", "#ingredients ul");
-  loadFileInto("equipment.html", "#equipment ul");
-  loadFileInto("directions.html", "#directions ol");
-  
-  
-  
-  
-  document.querySelector("#titleBanner h1").classList.add("tp");
-  
-  document.querySelector("#titleBanner h1").onclick = function() {    
-    this.classList.toggle("yellow");
-  }
-  
-  document.querySelector("#recipeColumns #ingredients").onclick = function() {
-    this.classList.toggle("tpShow")
-  }
-  
-  document.querySelector("#recipeColumns #equipment").onclick = function() {
-    this.classList.toggle("tpShow")
-  }
-  
-  document.querySelector("#recipeColumns #directions").onclick = function() {
-    this.classList.toggle("tpShow")
-  }
-  
-  setTimeout(function() {
-    document.querySelector("#recipeColumns #directions ol").innerHTML += "<li>Enjoy as you please!</li>";
-  }, 100);
-
+ 
   
   
   
